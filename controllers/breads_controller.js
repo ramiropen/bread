bbdd1a5baba79103cfed43fc120026b4166c7dfe
unknown <ramiropen.work@gmail.com>
2022-03-10 18:55:38 -0800

    update

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -24,29 +24,55 @@
 //     res.send(Bread[req.params.arrayIndex])
 //   })
   // -------------------------------------------------
-  const express = require('express')
-const breads = express.Router()
-const Bread = require('../models/bread.js')
+//   const express = require('express')
+// const breads = express.Router()
+// const Bread = require('../models/bread.js')
 
-// INDEX------changed INDEX
-// breads.get('/', (req, res) => {  
-//   res.render('index')
+// // INDEX------changed INDEX
+// // breads.get('/', (req, res) => {  
+// //   res.render('index')
+// // res.send(Bread)
+// // })
+
+// // INDEX
+// breads.get('/', (req, res) => {
+//   res.render('Index',
+//     {
+//       breads: Bread
+//     }
+//   )
 // res.send(Bread)
 // })
 
-// INDEX
-breads.get('/', (req, res) => {
-  res.render('Index',
-    {
-      breads: Bread
-    }
-  )
-res.send(Bread)
-})
+// module.exports = breads
 
-module.exports = breads
+// // SHOW
+// breads.get('/:arrayIndex', (req, res) => {
+//     res.send(Bread[req.params.arrayIndex])
+//   })
+// ---------------------------------------------------
+
+const express = require("express");
+const breads = express.Router();
+const Bread = require("../models/bread.js");
+
+// INDEX
+breads.get("/", (req, res) => {
+  res.render("Index", {
+    breads: Bread,
+    title: "Index Page",
+  });
+});
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+breads.get("/:arrayIndex", (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
+    });
+  } else {
+    res.render("error404");
+  }
+});
+
+module.exports = breads;
